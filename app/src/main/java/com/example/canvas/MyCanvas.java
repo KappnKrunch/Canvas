6e0a8db26459e2d3f2eef2a743f63b444d9fd2d5
markf <marksf993@gmail.com>
2019-12-29 17:12:08 -0800

    Added functionality to switch between sharps and flats Added visualization of internal connections between notes

--- app/src/main/java/com/example/canvas/MyCanvas.java
+++ app/src/main/java/com/example/canvas/MyCanvas.java
@@ -22,6 +22,7 @@ public class MyCanvas extends View
     float[] yVerts = new float[12];
 
     String[] musicalNotes = {"C","G","D","A","E","B","Gb","Db","Ab","Eb","Bb","F"}; //order which they appear on the circle
+    String[] musicalNotes2 = {"C","G","D","A","E","B","F#","C#","G#","D#","A#","F"};
     String[] keyBoardNoteOrder = {"C","Db","D","Eb","E","F","Gb","G","Ab","A","Bb","B"}; //order which they appear on the keyboard
 
     RectF[][] keys = new RectF[24][2]; //each keys contains multiple rectangles
@@ -35,7 +36,11 @@ public class MyCanvas extends View
     RectF cycleChordProgressionForwardButton;
     RectF cycleChordProgressionBackwardButton;
 
-    ChordPlayer chordPlayer;
+    boolean sharps = true;
+    boolean showInternalRelations = false;
+
+    RectF sharpsOrFlatsButton;
+    RectF showInternalRelationsButton;
 
 
     public MyCanvas(Context context)
@@ -44,8 +49,6 @@ public class MyCanvas extends View
 
         paint = new Paint();
         paint.setAntiAlias(true);
-
-        chordPlayer = new ChordPlayer();
     }
 
 
@@ -78,7 +81,6 @@ public class MyCanvas extends View
         }
         else{
             chords.add(newChord);
-
         }
 
 
@@ -106,6 +108,97 @@ public class MyCanvas extends View
 
     }
 
+    private String GetNoteFromVertex(float xPos, float yPos)
+    {
+        ArrayList<Integer> noteIndexX = new ArrayList<>();
+        ArrayList<Integer> noteIndexY = new ArrayList<>();
+
+        for(int i = 0; i < 12; i++)
+        {
+            if( xVerts[i] == xPos){
+                noteIndexX.add(i);
+
+                //Log.d("noteIndexX",String.valueOf(i));
+            }
+
+            if( yVerts[i] == yPos){
+                noteIndexY.add(i);
+
+                //Log.d("noteIndexY",String.valueOf(i));
+            }
+        }
+
+        //Log.d("noteIndex",String.valueOf(noteIndexX.size()));
+
+        ArrayList<Integer> finalNoteIndex = new ArrayList<>();
+
+        for(int i =0; i < noteIndexX.size();i++)
+        {
+            if(noteIndexY.contains(noteIndexX.get(i)))
+            {
+                finalNoteIndex.add(noteIndexX.get(i));
+            }
+        }
+
+        int noteIndex = finalNoteIndex.get(0);
+
+        return musicalNotes[noteIndex];
+    }
+
+    private int GetNoteIndexFromString(String note)
+    {
+        int returnIndex = -1;
+
+        for(int i = 0; i < keyBoardNoteOrder.length; i++)
+        {
+            if(note == keyBoardNoteOrder[i])
+            {
+                returnIndex = i;
+            }
+        }
+
+        if(returnIndex == -1)
+        {
+            Log.e("GetNoteFromString","InvalidNoteName");
+        }
+
+        return returnIndex;
+    }
+
+    private void DrawArrowhead(Canvas canvas, float xPos, float yPos, float fromX, float fromY, float toX, float toY, Paint paint)
+    {
+        float deltaX = fromX - toX;
+        float deltaY = fromY - toY;
+        float thetaRadians = (float)Math.atan2(deltaY,deltaX) - 3.14159f*0.5f;
+        float size = 20;
+
+        float topVertexX = (0f * (float)Math.cos(thetaRadians)) - (size*0.5f * (float)Math.sin(thetaRadians));
+        float topVertexY = (size*0.5f*(float)Math.cos(thetaRadians)) + (0f*(float)Math.sin(thetaRadians));
+
+        float rightVertexX = (size*0.5f * (float)Math.cos(thetaRadians)) - (-size*0.5f * (float)Math.sin(thetaRadians));
+        float rightVertexY = (-size*0.5f * (float)Math.cos(thetaRadians)) + (size*0.5f * (float)Math.sin(thetaRadians));
+
+        float leftVertexX = (-size*0.5f * (float)Math.cos(thetaRadians)) - (-size*0.5f * (float)Math.sin(thetaRadians));
+        float leftVertexY = (-size*0.5f * (float)Math.cos(thetaRadians)) + (-size*0.5f * (float)Math.sin(thetaRadians));
+
+
+        //Log.d("DrawArrowhead",String.valueOf(xPos)+","+String.valueOf(yPos)+"\n"+String.valueOf(leftVertexX)+","+String.valueOf(leftVertexY));
+
+        Path arrowHeadPath = new Path();
+
+        arrowHeadPath.moveTo(topVertexX+xPos,topVertexY+yPos);
+        arrowHeadPath.lineTo(leftVertexX+xPos,leftVertexY+yPos);
+        arrowHeadPath.lineTo(rightVertexX+xPos,rightVertexY+yPos);
+        arrowHeadPath.lineTo(topVertexX+xPos,topVertexY+yPos);
+
+        //canvas.drawCircle(topVertexX+xPos,topVertexY+yPos,3,paint);
+        //canvas.drawCircle(leftVertexX,leftVertexY,5,paint);
+        //canvas.drawCircle(rightVertexX,rightVertexY,5,paint);
+
+        canvas.drawPath(arrowHeadPath,paint);
+    }
+
+
     private float[] getVertices(String note)
     {
         //currently not working find a way to search arrays
@@ -129,23 +222,17 @@ public class MyCanvas extends View
 
     private void DrawCO5(Canvas canvas)
     {
-        paint.setColor(Color.WHITE);
-        paint.setStyle(Paint.Style.FILL);
-
-        canvas.drawRect(0f,0f,canvas.getWidth(), canvas.getHeight(), paint );
-
         paint.setColor(Color.BLACK);
         paint.setStrokeWidth(canvas.getWidth() * 0.01618f * 0.5f);
         paint.setStyle(Paint.Style.STROKE);
 
+        if(showInternalRelations){paint.setColor(Color.WHITE);}
 
-
-        float radius = canvas.getWidth() / (1.618f*2);
+        float radius = canvas.getWidth() / (1.618f*1.5f);
         float centerX = canvas.getWidth()/2;
-        float centerY = canvas.getHeight()/3;
+        float centerY = canvas.getHeight()/2.8f;
 
         canvas.drawCircle(centerX,centerY,radius,paint);
-
 
         for(int i = 0; i < 12; i++)
         {
@@ -167,14 +254,24 @@ public class MyCanvas extends View
             canvas.drawCircle(newX,newY,10,paint);
 
 
-            float newTextX = (float)(centerX + radius * Math.sin(percentAroundCircle) * 1.3f);
-            float newTextY = (float)(centerY - radius * Math.cos(percentAroundCircle) * 1.3f);
+            float newTextX = (float)(centerX + radius * Math.sin(percentAroundCircle) * 1.15f);
+            float newTextY = (float)(centerY - radius * Math.cos(percentAroundCircle) * 1.15f);
+
+            float textHeightOffset = (paint.ascent() + paint.descent()) * 0.5f;
+            newTextY -= textHeightOffset;
 
             paint.setStyle(Paint.Style.FILL);
             paint.setTextSize(50);
             paint.setTextAlign(Paint.Align.CENTER);
 
-            canvas.drawText(musicalNotes[i],newTextX,newTextY,paint);
+            if(sharps)
+            {
+                canvas.drawText(musicalNotes2[i],newTextX,newTextY,paint);
+            }
+            else
+            {
+                canvas.drawText(musicalNotes[i],newTextX,newTextY,paint);
+            }
         }
     }
 
@@ -289,30 +386,8 @@ public class MyCanvas extends View
         canvas.drawRect(0,canvas.getHeight() * (3.55f/5), canvas.getWidth(), canvas.getHeight() * (3.75f/5), paint);
     }
 
-    private void DrawNotes(Canvas canvas)
+    private void DrawCO5Shape(Canvas canvas, float[] noteXVertices, float[] noteYVertices, int vertexCount)
     {
-        float[] noteXVertices = new float[12];
-        float[] noteYVertices = new float[12];
-
-        int vertexCount = 0;
-
-        int j = 0; //special index for maintaining proper sequencing
-
-        for(int i = 0 ; i < 12; i++)
-        {
-            if( activeKeys[j] == 1 | activeKeys[j+12] == 1)
-            {
-                float[] vertex = getVertices(keyBoardNoteOrder[j]);
-
-                noteXVertices[vertexCount] = vertex[0];
-                noteYVertices[vertexCount] = vertex[1];
-
-                vertexCount++;
-            }
-
-            j = (j+7)%12; //does the indexing by intervals of 7 because thats how its displayed on the circle
-        }
-
         paint.setColor(Color.LTGRAY);
         paint.setStrokeWidth(5);
         paint.setStyle(Paint.Style.FILL);
@@ -338,10 +413,140 @@ public class MyCanvas extends View
         canvas.drawPath(co5Shape,paint);
     }
 
+    private String IntervalBetweenNotes(String note1, String note2)
+    {
+        String[] intervls = {   "O","m2\nM7","M2\nm7","m3\nM6","M3\nm6","4\n5","T",
+                                "5\n4","m6\nM3","M6\nm3","m7\nM2","M7\nm2"  };
+
+        int interval = (GetNoteIndexFromString(note1) - GetNoteIndexFromString(note2) + 12)%12;
+
+        return intervls[interval];
+    }
+
+
+    private void DrawCO5Relations(Canvas canvas, float[] noteXVertices, float[] noteYVertices, int vertexCount)
+    {
+
+        if(showInternalRelations & chords.size() > 0)
+        {
+            float[] nextNoteXVertices = new float[12];
+            float[] nextNoteYVertices = new float[12];
+            int newVertexCount = 0;
+            int j = 0;
+
+            for(int i = 0 ; i < 12; i++)
+            {
+                //if key is down add note to vertex list
+                //two notes on the keyboard account for one note in the circle
+                if( chords.get((activeChordIndex + chords.size() - 1)%chords.size())[j] == 1 | chords.get((activeChordIndex + chords.size() - 1)%chords.size())[j+12] == 1)
+                {
+                    float[] vertex = getVertices(keyBoardNoteOrder[j]);
+
+                    nextNoteXVertices[newVertexCount] = vertex[0];
+                    nextNoteYVertices[newVertexCount] = vertex[1];
+
+                    newVertexCount++;
+                }
+
+                j = (j+7)%12; //does the indexing by intervals of 7 because thats how its displayed on the circle
+            }
+
+            //change the paint settings
+
+            paint.setColor(Color.BLACK);
+            paint.setStrokeWidth(5);
+            paint.setStyle(Paint.Style.FILL);
+            paint.setTextAlign(Paint.Align.CENTER);
+
+            //go through each vertex of the first chord and map/name its relation to the next chord
+            for(int i = 0; i < vertexCount; i++)
+            {
+                for(int k = 0; k < newVertexCount; k++)
+                {
+                    paint.setColor(0xfffd837b);
+                    canvas.drawLine(noteXVertices[i],noteYVertices[i],nextNoteXVertices[k],nextNoteYVertices[k],paint);
+
+
+                    float arrowPointX = (noteXVertices[i]*7 + nextNoteXVertices[k]) /8;
+                    float arrowPointY = (noteYVertices[i]*7 + nextNoteYVertices[k]) /8;
+
+                    DrawArrowhead(canvas,arrowPointX,arrowPointY,noteXVertices[i],noteYVertices[i],nextNoteXVertices[k],nextNoteYVertices[k],paint);
+                }
+
+                for(int k = 0; k < newVertexCount; k++)
+                {
+                    paint.setColor(Color.BLACK);
+                    paint.setTextSize(35);
+
+                    canvas.drawCircle(nextNoteXVertices[k],nextNoteYVertices[k],10,paint);
+
+                    float midPointX = (noteXVertices[i]*3 + nextNoteXVertices[k]) /4;
+                    float midPointY = (noteYVertices[i]*3 + nextNoteYVertices[k]) /4;
+
+                    float textHeightOffset = (paint.ascent() + paint.descent()) * 0.5f;
+
+                    String noteA = GetNoteFromVertex(noteXVertices[i],noteYVertices[i]);
+                    String noteB = GetNoteFromVertex(nextNoteXVertices[k],nextNoteYVertices[k]);
+
+                    String intervalBetweenNotes = IntervalBetweenNotes(noteA,noteB);
+
+                    if( intervalBetweenNotes.split("\n").length > 1)
+                    {
+                        String upperInterval = intervalBetweenNotes.split("\n")[0];
+                        String lowerInterval = intervalBetweenNotes.split("\n")[1];
+
+                        canvas.drawText(upperInterval,midPointX,midPointY - 5f,paint);
+                        canvas.drawText(lowerInterval,midPointX,midPointY - (textHeightOffset*2) + 5f,paint);
+                    }
+                    else
+                    {
+                        String upperInterval = intervalBetweenNotes.split("\n")[0];
+                        canvas.drawText(upperInterval,midPointX,midPointY- textHeightOffset,paint);
+                    }
+                }
+            }
+        }
+
+    }
+
+
+    private void DrawCO5Notes(Canvas canvas)
+    {
+        float[] noteXVertices = new float[12];
+        float[] noteYVertices = new float[12];
+
+        int vertexCount = 0;
+
+        int j = 0; //special index for maintaining proper sequencing
+
+        for(int i = 0 ; i < 12; i++)
+        {
+            //if key is down add note to vertex list
+            //two notes on the keyboard account for one note in the circle
+            if( activeKeys[j] == 1 | activeKeys[j+12] == 1)
+            {
+                float[] vertex = getVertices(keyBoardNoteOrder[j]);
+
+                noteXVertices[vertexCount] = vertex[0];
+                noteYVertices[vertexCount] = vertex[1];
+
+                vertexCount++;
+            }
+
+            j = (j+7)%12; //does the indexing by intervals of 7 because thats how its displayed on the circle
+        }
+
+        DrawCO5Shape(canvas, noteXVertices, noteYVertices, vertexCount);
+
+        DrawCO5Relations(canvas, noteXVertices, noteYVertices, vertexCount);
+
+    }
+
     private void DrawUI(Canvas canvas)
     {
         paint.setTextSize(37.5f);
         paint.setStrokeWidth(3);
+
 
 
         addChordButton = new RectF(180,canvas.getHeight()*(9f/10),280,canvas.getHeight()*(9f/10)+ 120);
@@ -375,6 +580,7 @@ public class MyCanvas extends View
         canvas.drawText("-",90,canvas.getHeight()*(9f/10) + 75 , paint);
 
 
+
         cycleChordProgressionBackwardButton = new RectF(360,canvas.getHeight()*(9f/10),460,canvas.getHeight()*(9f/10)+ 120);
 
         paint.setColor(Color.LTGRAY);
@@ -390,6 +596,7 @@ public class MyCanvas extends View
         canvas.drawText("<",410,canvas.getHeight()*(9f/10) + 75 , paint);
 
 
+
         cycleChordProgressionForwardButton = new RectF(500,canvas.getHeight()*(9f/10),600,canvas.getHeight()*(9f/10) + 120);
 
         paint.setColor(Color.LTGRAY);
@@ -403,6 +610,49 @@ public class MyCanvas extends View
         canvas.drawRect(cycleChordProgressionForwardButton,paint);
 
         canvas.drawText(">",550,canvas.getHeight()*(9f/10) + 75 , paint);
+
+
+
+        showInternalRelationsButton = new RectF(canvas.getWidth() - 280,canvas.getHeight()*(9f/10),canvas.getWidth() - 180,canvas.getHeight()*(9f/10) + 120);
+
+        if(showInternalRelations)
+        {
+            paint.setColor(Color.DKGRAY);
+        }
+        else
+        {
+            paint.setColor(Color.LTGRAY);
+        }
+
+        paint.setStyle(Paint.Style.FILL);
+
+        canvas.drawRect(showInternalRelationsButton,paint);
+
+        paint.setColor(Color.BLACK);
+        paint.setStyle(Paint.Style.STROKE);
+
+        canvas.drawRect(showInternalRelationsButton,paint);
+
+        canvas.drawText("R",canvas.getWidth() - 230,canvas.getHeight()*(9f/10) + 75 , paint);
+
+
+
+        sharpsOrFlatsButton = new RectF(10,10,110,110);
+
+        paint.setColor(Color.LTGRAY);
+        paint.setStyle(Paint.Style.FILL);
+
+        canvas.drawRect(sharpsOrFlatsButton,paint);
+
+        paint.setColor(Color.BLACK);
+        paint.setStyle(Paint.Style.STROKE);
+
+        canvas.drawRect(sharpsOrFlatsButton,paint);
+
+        String sharpsOrFlats = "#";
+        if(sharps){sharpsOrFlats = "b";}
+
+        canvas.drawText(sharpsOrFlats,60,70, paint);
 
     }
 
@@ -432,6 +682,18 @@ public class MyCanvas extends View
         if(cycleChordProgressionBackwardButton.contains(event.getX(),event.getY()))
         {
             SwitchChords(-1);
+            UIUpdated = true;
+        }
+
+        if(showInternalRelationsButton.contains(event.getX(),event.getY()))
+        {
+            showInternalRelations = !showInternalRelations;
+            UIUpdated = true;
+        }
+
+        if(sharpsOrFlatsButton.contains(event.getX(),event.getY()))
+        {
+            sharps = !sharps;
             UIUpdated = true;
         }
 
@@ -465,9 +727,14 @@ public class MyCanvas extends View
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
 
+        paint.setColor(Color.WHITE);
+        paint.setStyle(Paint.Style.FILL);
+
+        canvas.drawRect(0f,0f,canvas.getWidth(), canvas.getHeight(), paint );
+
         DrawCO5(canvas);
         DrawKeyBoard(canvas);
-        DrawNotes(canvas);
+        DrawCO5Notes(canvas);
         DrawUI(canvas);
 
         //chordPlayer.PlayChord(activeKeys,0.5f);
